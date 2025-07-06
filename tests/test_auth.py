079a066f769363f55d02762ccccc56ccca81abf7
Ben Nuttall <ben@bennuttall.com>
2025-07-06 22:33:42 +0100

    wip

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -55,25 +55,11 @@ def auth_response_with_invalid_body() -> Mock:
     )
 
 
-@patch("hostedpi.auth.datetime")
-def test_auth_with_no_args(mock_datetime, mock_dt, auth_response, auth_response_2, auth_url):
+@patch("hostedpi.auth.get_settings")
+def test_auth_init_with_no_args(get_settings, settings):
+    get_settings.return_value = settings
     auth = MythicAuth()
-    mock_datetime.now.return_value = mock_dt
-    auth._auth_session.post.side_effect = [auth_response, auth_response_2]
-
     assert repr(auth) == "<MythicAuth id=test_id>"
-
-    assert auth.token == "foobar"
-    assert auth._token_expiry == mock_dt + timedelta(seconds=3600)
-    assert auth.session.headers["Authorization"] == "Bearer foobar"
-
-    mock_datetime.now.return_value = mock_dt + timedelta(seconds=3601)
-    assert auth.token == "barfoo"
-    assert auth.session.headers["Authorization"] == "Bearer barfoo"
-
-    assert auth._auth_session.post.call_count == 2
-    assert auth._auth_session.post.call_args_list[0][0][0] == auth_url
-    assert auth._auth_session.post.call_args_list[1][0][0] == auth_url
 
 
 @patch("hostedpi.auth.datetime")
